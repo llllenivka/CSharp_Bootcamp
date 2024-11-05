00685be2f62d825312d43e96f2563470dc7b03c4
llllenivka <adelisha.dinov@gmail.com>
2024-11-05 13:08:38 +0300

    add: parsing file data

--- Project01/src/exercise2/Program.cs
+++ Project01/src/exercise2/Program.cs
@@ -8,14 +8,12 @@ class Program
     }
     
     public class LinearAlgebraicEquation {
-        private string ? filename = "1.txt";
+        public string filename = "1.txt";  // public string filename { get; set; }
         List<List<int>> matrix;
 
         public void Calculation() {
-            // filename = Console.ReadLine(); ПОТОМ ДОБАВЬ!!!!!!!!!!!!
-            if(!File.Exists(filename) || string.IsNullOrEmpty(filename)) {
-                OutputUser.ErrorFile();
-            }
+            if(!ReadMatrixFromFile.IsCorrectFile(filename)) return;
+            ReadMatrixFromFile.ReadMatrix(filename);
         }
     }
 
@@ -24,6 +22,49 @@ class Program
         public static void ErrorFile() => Console.WriteLine("Input error. File isn't exist");
         public static void ErrorCalculation() => Console.WriteLine("The system of linear algebraic equations has no solutions");
         public static void ErrorNumber() => Console.WriteLine("Couldn't parse a number. Please, try again");
+    }
+
+    public class ReadMatrixFromFile {
+        public static bool  IsCorrectFile(string ? filename) {
+            bool code = true;
+            // filename = Console.ReadLine(); ПОТОМ ДОБАВЬ!!!!!!!!!!!!
+            if(!File.Exists(filename)) {
+                OutputUser.ErrorFile();
+                code = false;
+            }
+            
+            return code;
+        }
+        
+        public static string[] ReadString(string filename) {
+            return File.ReadAllLines(filename);
+        }
+
+        public static List<List<int>> ReadMatrix(string filename) {
+            List<List<int>> matrix = new List<List<int>>();
+            string[] matrixString = ReadString(filename);
+
+            foreach(var line in matrixString) {
+                matrix.Add(PushMatrix(line));
+            }
+            return matrix;
+        }
+
+        private static List<int> PushMatrix(string line) {
+            List<int> lineOfMatrix = new List<int>();
+            string[] splitLine = line.Split(" ");
+
+            foreach(var numberString in splitLine) {
+                int number;
+                if(!int.TryParse(numberString, out  number)) {
+                    OutputUser.ErrorNumber();
+                } else {
+                    lineOfMatrix.Add(number);
+                }
+            }
+
+            return lineOfMatrix;
+        }
     }
 
 
